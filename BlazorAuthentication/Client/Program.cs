c02d1fcf3d1f6fc8866959475e83330d1fc6f496
Julia Korzeniewska <juliiaak96@gmail.com>
2024-01-28 21:57:25 +0100

    Projekt

--- BlazorAuthentication/Client/Program.cs
+++ BlazorAuthentication/Client/Program.cs
@@ -1,8 +1,5 @@
-global using BlazorEcommerce.Shared;
-global using System.Net.Http.Json;
-global using BlazorEcommerce.Client.Services.ProductService;
-global using BlazorEcommerce.Client.Services.CategoryService;
-using BlazorEcommerce.Client;
+global using Microsoft.AspNetCore.Components.Authorization;
+using BlazorAuthentication.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -11,7 +8,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IProductService, ProductService>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 await builder.Build().RunAsync();
